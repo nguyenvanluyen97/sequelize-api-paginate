9eb2ccbec0a7185d4a5cecdc017e91f01da168bb
LuyenNguyen <luyennguyen@meu-solutions.com>
2021-04-13 18:14:30 +0700

    update index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const queryModel = require('./src/query-model');
+const middleProcess = require('./src/middle');
+
+module.exports = {
+    query = queryModel,
+    middle = middleProcess
+}
