b89fa4714bb9c08e42c9feb6fcfb838f45072e2e
LuyenNguyen <luyennguyen@meu-solutions.com>
2021-09-20 14:41:17 +0700

    add query includes

--- src/query-model.js
+++ src/query-model.js
@@ -1,14 +1,16 @@
 'use strict'
 
-module.exports = async function(model,payload) {
+module.exports = async function (model, payload, includeModels = []) {
     var result = await model.findAndCountAll({
         limit: payload.pageSize,
         offset: (payload.currentPage - 1) * payload.pageSize || 0,
         order: payload.sortField != null ? [
             [payload.sortField, payload.sortOrder]
         ] : [],
+        include: includeModels,
         where: payload.filters,
-        raw: true
+        raw: true,
+        nest: true
     });
     result['totalPages'] = Math.ceil(result['count'] / payload.pageSize);
     result['currentPage'] = payload.currentPage;
